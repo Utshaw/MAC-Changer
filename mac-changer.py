dd5da2c57d2a726327a887ab70ccb3b497deb286
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-06-04 03:48:53 -0400

    Update program use function to make things modular

--- mac-changer.py
+++ mac-changer.py
@@ -1,11 +1,30 @@
 import subprocess
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser() 
+    parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New  MAC address")
+    (options, arguments) =  parser.parse_args() # allows the object to undrstand what the user entered
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info")
+    else:
+        return options
 
 
-interface = "eth0"
-new_mac = "00:11:22:33:44:66"
 
-print('[+] Changing MAC address for ' + interface  + " to " + new_mac)
+def change_mac(interface,new_mac):
+    print('[+] Changing MAC address for ' + interface  + " to " + new_mac)
 
-subprocess.call('ifconfig ' + interface + ' down', shell=True)
-subprocess.call('ifconfig ' + interface + ' hw ether ' + new_mac, shell=True)
-subprocess.call('ifconfig ' + interface + ' up', shell=True)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
+
+
+
