8ed3b448fc355bdf813be5f044d278569e909ed9
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-06-03 23:54:41 -0400

    Add py

---
+++ mac-changer.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+interface = "eth0"
+new_mac = "00:11:22:33:44:66"
+
+print('[+] Changing MAC address for ' + interface  + " to " + new_mac)
+
+subprocess.call('ifconfig ' + interface + ' down', shell=True)
+subprocess.call('ifconfig ' + interface + ' hw ether ' + new_mac, shell=True)
+subprocess.call('ifconfig ' + interface + ' up', shell=True)
